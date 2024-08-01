3769092803b3dff1272d833fef67ddffb2bf7c06
MohamedAazil <142242204+MohamedAazil@users.noreply.github.com>
2024-08-02 04:01:28 +0530

    Update settings.py
    added whitenoise to middle ware

--- idea_project/idea_project/settings.py
+++ idea_project/idea_project/settings.py
@@ -26,7 +26,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
